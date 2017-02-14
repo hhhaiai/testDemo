3769a87cb8ce59cb54d1c3b1ad99d8314f43074d
yongfu.xyf <yongfu.xyf@alibaba-inc.com>
2017-02-14 12:43:52 +0800

    增加获取栈顶界面文件. 增加蓝牙/wifi信息获取通道

---
+++ app/src/main/java/com/test/AppUtils.java
@@ -0,0 +1,66 @@
+package com.test;
+
+import android.app.Application;
+import android.app.Instrumentation;
+import android.content.Context;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
+/**
+ * Copyright © 2017 Umeng Inc. All rights reserved.
+ * Description: TODO
+ * Version: 1.0
+ * Create: 17/2/14 11:14
+ * Author: sanbo
+ */
+public class AppUtils {
+    public static void getNowActivity(Application app) {
+        Field f;
+        Object thread = null;
+        Instrumentation base;
+        // Replace instrumentation
+        try {
+            thread = getActivityThread(app);
+            f = thread.getClass().getDeclaredField("mInstrumentation");
+            f.setAccessible(true);
+            base = (Instrumentation) f.get(thread);
+            if (base != null) {
+                //get instrumentation activity list
+                List<Instrumentation.ActivityMonitor> mActivityMonitors;
+                f = Instrumentation.class.getDeclaredField("mActivityMonitors");
+                f.setAccessible(true);
+                mActivityMonitors = (List<Instrumentation.ActivityMonitor>) f.get(base);
+                for (int i = 0; i < mActivityMonitors.size(); i++) {
+                    Instrumentation.ActivityMonitor activityMonitor = mActivityMonitors.get(i);
+                    //获取Activity名称
+                    activityMonitor.getLastActivity().getClass().getName();
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to replace instrumentation for thread: " + thread);
+        }
+    }
+
+    private static Object getActivityThread(Context context) {
+        try {
+            Class activityThread = Class.forName("android.app.ActivityThread");
+            // ActivityThread.currentActivityThread()
+            Method m = activityThread.getMethod("currentActivityThread", new Class[0]);
+            m.setAccessible(true);
+            Object thread = m.invoke(null, new Object[0]);
+            if (thread != null) return thread;
+
+            // context.@mLoadedApk.@mActivityThread
+            Field mLoadedApk = context.getClass().getField("mLoadedApk");
+            mLoadedApk.setAccessible(true);
+            Object apk = mLoadedApk.get(context);
+            Field mActivityThreadField = apk.getClass().getDeclaredField("mActivityThread");
+            mActivityThreadField.setAccessible(true);
+            return mActivityThreadField.get(apk);
+        } catch (Throwable ignore) {
+            throw new RuntimeException("Failed to get mActivityThread from context: " + context);
+        }
+    }
+}
--- app/src/main/java/com/test/MainActivity.java
+++ app/src/main/java/com/test/MainActivity.java
@@ -1,25 +1,28 @@
 package com.test;
 
 import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.Uri;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
-import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Locale;
 
 import static com.test.R.*;
 
@@ -32,8 +35,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
-
-
     }
 
 
@@ -50,15 +51,254 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.btn2:
                 Log.d(T, "B");
+                getBluetoothAndWifoInfo();
                 break;
             case R.id.btn3:
                 Log.d(T, "C");
                 break;
             default:
-                break;
+                break;g
         }
 
 
+    }
+
+    /**************************************************************************************
+     * ******************************* 蓝牙/无线相关的,需要部分权限 ***************************
+     **************************************************************************************/
+    private void getBluetoothAndWifoInfo() {
+
+
+        /**
+         * 需要权限  <uses-permission android:name="android.permission.BLUETOOTH" />
+         */
+        BluetoothAdapter bAdapt = BluetoothAdapter.getDefaultAdapter();
+        //蓝牙地址
+        String bluetoothAddr = bAdapt.getAddress();
+        //Bluetooth name
+        String bluetoothName = bAdapt.getName();
+
+        //蓝牙状态
+        String bluetoothState = "idle";
+        switch (bAdapt.getState()) {
+            case BluetoothAdapter.STATE_ON:
+                bluetoothState = "state_on";
+                break;
+            case BluetoothAdapter.STATE_OFF:
+                bluetoothState = "state_off";
+                break;
+            case BluetoothAdapter.STATE_CONNECTED:
+                bluetoothState = "state_connected";
+                break;
+            case BluetoothAdapter.STATE_CONNECTING:
+                bluetoothState = "state_connecting";
+                break;
+            case BluetoothAdapter.STATE_DISCONNECTED:
+                bluetoothState = "state_disconnected";
+                break;
+            case BluetoothAdapter.STATE_DISCONNECTING:
+                bluetoothState = "state_disconnecting";
+                break;
+            case BluetoothAdapter.STATE_TURNING_OFF:
+                bluetoothState = "state_turning_off";
+                break;
+            case 14:
+                bluetoothState = "state_ble_turning_on";
+                break;
+            case 15:
+                bluetoothState = "state_ble_on";
+                break;
+            case 16:
+                bluetoothState = "state_ble_turning_off";
+                break;
+            default:
+                break;
+        }
+
+        /**
+         * 需要权限 <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
+         */
+        WifiManager mWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = mWifi.getConnectionInfo();
+        //连接wifi的地址
+        String bssidName = wifiInfo.getBSSID();
+        //连接wifi名字
+        String ssidName = wifiInfo.getSSID();
+        //mac地址
+        String wifiMac = wifiInfo.getMacAddress();
+
+        //当前频率
+        int frequency = -1;
+        if (Build.VERSION.SDK_INT > 20) {
+            try {
+                frequency = wifiInfo.getFrequency();
+            } catch (Throwable e) {
+
+            }
+        }
+        //int ip值
+        int ip = wifiInfo.getIpAddress();
+        //当前链接速度
+        int linkSpeed = wifiInfo.getLinkSpeed();
+        //是否为隐藏wifi
+        boolean isHiddenSSID = wifiInfo.getHiddenSSID();
+        //当前网络信号,单位dBm
+        int rssi = wifiInfo.getRssi();
+        //网络ID
+        int networkID = wifiInfo.getNetworkId();
+
+        String macAddress = android.provider.Settings.Secure.getString(getContentResolver(), "bluetooth_address");
+
+        /**
+         * 结果拼接
+         */
+        String s = "===============蓝牙信息==============="
+                + "\n蓝牙名字:" + bluetoothName
+                + "\n蓝牙MAC[官方API]:" + bluetoothAddr
+                + "\n蓝牙MAC[系统数据库方式获取]:" + macAddress
+                + "\n蓝牙状态:" + bluetoothState
+                + "\n===============无线信息==============="
+                + "\n无线名字:" + ssidName
+                + "\n无线MAC[官方API获取]:" + wifiMac
+                + "\n无线MAC[javaAPI获取]:" + getMacByJavaAPI()
+                + "\nBssid:" + bssidName
+                + "\n当前频率:" + frequency
+                + "\nWIFI信道[系统API获取]:" + getCurrentChannel()
+                + "\nWIFI信道[根据频率获取]:" + getChannelByFrequency(frequency)
+                + "\nip:" + longToIp(ip)
+                + "\n当前链接速度:" + linkSpeed
+                + "\n是否为隐藏wifi:" + isHiddenSSID
+                + "\n信号强度:" + rssi + "dBm\n网络ID:" + networkID;
+        showMessage(s);
+
+    }
+
+
+    /**
+     * 获取系统MAC地址.需要权限<uses-permission android:name="android.permission.INTERNET" />
+     *
+     * @return
+     */
+    @TargetApi(9)
+    private String getMacByJavaAPI() {
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface netInterface = interfaces.nextElement();
+                if ("wlan0".equals(netInterface.getName()) || "eth0".equals(netInterface.getName())) {
+                    byte[] addr = netInterface.getHardwareAddress();
+                    if (addr == null || addr.length == 0) {
+                        return null;
+                    }
+                    StringBuilder buf = new StringBuilder();
+                    for (byte b : addr) {
+                        buf.append(String.format("%02X:", b));
+                    }
+                    if (buf.length() > 0) {
+                        buf.deleteCharAt(buf.length() - 1);
+                    }
+                    return buf.toString().toLowerCase(Locale.getDefault());
+                }
+            }
+        } catch (Throwable e) {
+        }
+        return null;
+    }
+
+    public String longToIp(long i) {
+
+        return ((i >> 24) & 0xFF) +
+                "." + ((i >> 16) & 0xFF) +
+                "." + ((i >> 8) & 0xFF) +
+                "." + (i & 0xFF);
+
+    }
+
+
+    public int getCurrentChannel() {
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();// 当前wifi连接信息
+        List<ScanResult> scanResults = wifiManager.getScanResults();
+        for (ScanResult result : scanResults) {
+            if (result.BSSID.equalsIgnoreCase(wifiInfo.getBSSID())
+                    && result.SSID.equalsIgnoreCase(wifiInfo.getSSID()
+                    .substring(1, wifiInfo.getSSID().length() - 1))) {
+                return getChannelByFrequency(result.frequency);
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 根据频率获得信道
+     *
+     * @param frequency
+     * @return
+     */
+    public int getChannelByFrequency(int frequency) {
+        int channel = -1;
+        switch (frequency) {
+            case 2412:
+                channel = 1;
+                break;
+            case 2417:
+                channel = 2;
+                break;
+            case 2422:
+                channel = 3;
+                break;
+            case 2427:
+                channel = 4;
+                break;
+            case 2432:
+                channel = 5;
+                break;
+            case 2437:
+                channel = 6;
+                break;
+            case 2442:
+                channel = 7;
+                break;
+            case 2447:
+                channel = 8;
+                break;
+            case 2452:
+                channel = 9;
+                break;
+            case 2457:
+                channel = 10;
+                break;
+            case 2462:
+                channel = 11;
+                break;
+            case 2467:
+                channel = 12;
+                break;
+            case 2472:
+                channel = 13;
+                break;
+            case 2484:
+                channel = 14;
+                break;
+            case 5745:
+                channel = 149;
+                break;
+            case 5765:
+                channel = 153;
+                break;
+            case 5785:
+                channel = 157;
+                break;
+            case 5805:
+                channel = 161;
+                break;
+            case 5825:
+                channel = 165;
+                break;
+        }
+        return channel;
     }
 
     /**************************************************************************************
@@ -72,7 +312,6 @@ public class MainActivity extends AppCompatActivity {
      * 获取电源相关的,注解广播接收器实现
      */
     private void getBattery() {
-        tv = (TextView) findViewById(id.textView);
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
         this.registerReceiver(mBroadcastReceiver, filter);
@@ -196,11 +435,11 @@ public class MainActivity extends AppCompatActivity {
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case MSG_REFRESH_UI:
+                    tv = (TextView) findViewById(id.textView);
                     tv.setText(msg.obj.toString());
                     break;
             }
         }
     };
-
 
 }
