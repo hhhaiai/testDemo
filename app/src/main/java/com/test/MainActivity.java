07d3ddeaa01f58e04c25a5f999dee1d085944550
yongfu.xyf <yongfu.xyf@alibaba-inc.com>
2017-02-14 14:37:55 +0800

    增加sim卡/手机信息获取

--- app/src/main/java/com/test/MainActivity.java
+++ app/src/main/java/com/test/MainActivity.java
@@ -1,11 +1,13 @@
 package com.test;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -15,10 +17,15 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.CellLocation;
+import android.telephony.NeighboringCellInfo;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
+import java.lang.reflect.Method;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
 import java.util.List;
@@ -50,16 +57,215 @@ public class MainActivity extends AppCompatActivity {
                 //getBatteryManagerByLargeThan21();
                 break;
             case R.id.btn2:
-                Log.d(T, "B");
+                /**
+                 * 获取蓝牙/wifi信息
+                 */
                 getBluetoothAndWifoInfo();
                 break;
             case R.id.btn3:
-                Log.d(T, "C");
+                /**
+                 * 获取SIM信息
+                 */
+                getSIMInfo();
                 break;
+            case R.id.btn4:
+                break;
+            case R.id.btn5:
+                break;
+            case R.id.btn6:
+                break;
+
             default:
-                break;g
+                break;
         }
 
+
+    }
+
+    /**************************************************************************************
+     * ******************************* SIM卡相关信息获取,需要部分权限 ***************************
+     **************************************************************************************/
+    private void getSIMInfo() {
+        /**
+         * 需要权限 android.permission.READ_PHONE_STATE
+         */
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String simState = "unknow";
+        //卡状态
+        switch (tm.getSimState()) {
+            case TelephonyManager.SIM_STATE_READY:
+                simState = "sim_state_ready";
+                break;
+            case TelephonyManager.SIM_STATE_ABSENT:
+                simState = "sim_state_absent";
+                break;
+            case TelephonyManager.SIM_STATE_PIN_REQUIRED:
+                simState = "sim_state_pin_required";
+                break;
+            case TelephonyManager.SIM_STATE_NETWORK_LOCKED:
+                simState = "sim_state_network_locked";
+                break;
+            case TelephonyManager.SIM_STATE_PUK_REQUIRED:
+                simState = "sim_state_puk_required";
+                break;
+            case TelephonyManager.SIM_STATE_UNKNOWN:
+                simState = "sim_state_unknown";
+                break;
+            default:
+                simState = "unknow";
+                break;
+        }
+        //sim卡国家码
+        String simCountryIso = tm.getSimCountryIso();
+        // MCC+MNC
+        String operator = tm.getSimOperator();
+        //服务提供名字(SPN)
+        String simOperatorName = tm.getSimOperatorName();
+        String simSerialNumber = "unknow";
+        //SIM卡序列号,需要权限android.Manifest.permission.READ_PHONE_STATE
+        if (checkPermission(this, Manifest.permission.READ_PHONE_STATE)) {
+            simSerialNumber = tm.getSimSerialNumber();
+        }
+        //订阅ID.
+        /**
+         *  获取imsi(MCC+MNC+MIN)
+         *      MCC：Mobile Country Code,移动国家码, 一般3位:中国为460
+         *      MNC:Mobile Network Code,移动网络码, 一般为2位,也有1位(小于10的都是1位),也有3位(美国卡):电信03,移动02,联通GSM01
+         *      MIN共有10位其结构如下:09+M0M1M2M3+ABCD 其中的M0M1M2M3和MDN号码中的H0H1H2H3可存在对应关系,ABCD四位为自由分配.
+         *          可以看出IMSI在MIN号码前加了MCC,可以区别出每个用户的来自的国家,因此可以实现国际漫游.
+         *          在同一个国家内,如果有多个CDMA运营商,可以通过MNC来进行区别.
+         */
+        String subscriberId = "unknow";
+        if (checkPermission(this, Manifest.permission.READ_PHONE_STATE)) {
+            subscriberId = tm.getSubscriberId();
+        }
+        int mcc = getResources().getConfiguration().mcc;
+        int mnc = getResources().getConfiguration().mnc;
+
+        /**
+         * 手机其他信息采集
+         */
+
+        String callState = "无活动";
+        switch (tm.getCallState()) {
+            case TelephonyManager.CALL_STATE_RINGING:
+                callState = "响铃";
+                break;
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+                callState = "摘机";
+                break;
+            case TelephonyManager.CALL_STATE_IDLE:
+                callState = "无活动";
+                break;
+            default:
+                callState = "无活动";
+                break;
+        }
+        /**
+         * 电话方位 需要权限 android.permission.ACCESS_COARSE_LOCATION</p>
+         * 请求位置更新，如果更新将产生广播，接收对象为注册LISTEN_CELL_LOCATION的对象，需要的permission名称为ACCESS_COARSE_LOCATION。</p>
+         * location.requestLocationUpdate();
+         */
+
+        String cellLoc = "获取失败";
+        if (checkPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            CellLocation loc = tm.getCellLocation();
+            cellLoc = loc.toString();
+        }
+        //device id
+        String imei ="unknow";
+        if (checkPermission(this, Manifest.permission.READ_PHONE_STATE)) {
+            imei = tm.getDeviceId();
+        }
+        //设备软件版本号
+        String softwareVersion = "unknow";
+        if (checkPermission(this, Manifest.permission.READ_PHONE_STATE)) {
+            softwareVersion = tm.getDeviceSoftwareVersion();
+        }
+        //手机号获取
+        String lineNum= "unknow";
+        if (checkPermission(this, Manifest.permission.READ_PHONE_STATE)) {
+            lineNum = tm.getLine1Number();
+        }
+        /**
+         * 当前移动终端附近移动终端的信息 需要权限android.permission.ACCESS_COARSE_LOCATION
+         */
+//        StringBuilder sb = new StringBuilder();
+//        if (checkPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+//            List<NeighboringCellInfo> infos = tm.getNeighboringCellInfo();
+//            for (NeighboringCellInfo info : infos) {
+//                // 获取小区号
+//                int cid = info.getCid();
+//                // 获取邻居小区LAC
+//                // LAC:
+//                // 位置区域码。为了确定移动台的位置，每个GSM/PLMN的覆盖区都被划分成许多位置区，LAC则用于标识不同的位置区。
+//                int lac = info.getLac();
+//                int networkType = info.getNetworkType();
+//                int mPsc = info.getPsc();
+//                // 获取邻居小区信号强度
+//                int rssi = info.getRssi();
+//                sb.append("小区号:").append(cid)
+//                        .append(";LAC:").append(lac)
+//                        .append(";networkType:").append(networkType)
+//                        .append(";mPsc:").append(mPsc)
+//                        .append(";小区信号强度:").append(rssi);
+//            }
+//        }
+
+        //注册的ISO国家注册码
+        String networkCountryIso = tm.getNetworkCountryIso();
+        //MCC+MNC
+        String networkOperator = tm.getNetworkOperator();
+        //(当前已注册的用户)的名字
+        String networkOperatorName = tm.getNetworkOperatorName();
+        //当前使用的网络类型
+        int networkType = tm.getNetworkType();
+        //手机类型
+        int phoneType = tm.getPhoneType();
+        //语音邮件号码
+        String VoiceMailNumber ="unknow";
+        if (checkPermission(this, Manifest.permission.READ_PHONE_STATE)) {
+            VoiceMailNumber = tm.getVoiceMailNumber();
+        }
+        //ICC卡是否存在
+        boolean hasIccCard = tm.hasIccCard();
+        //是否漫游
+        boolean isNetworkRoaming = false;
+        if (checkPermission(this, Manifest.permission.READ_PHONE_STATE)) {
+            isNetworkRoaming = tm.isNetworkRoaming();
+        }
+        //数据活动状态
+        int dataActivity = tm.getDataActivity();
+        //数据连接状态
+        int dataState = tm.getDataState();
+        String s = "================SIM卡相关信息=================="
+                + "\nSIM状态:" + simState
+                + "\nSIM国家码:" + simCountryIso
+                + "\nSIM[mcc+mnc]:" + operator
+                + "\n服务提供名字(SPN):" + simOperatorName
+                + "\nSIM卡序列号(ICCID):" + simSerialNumber
+                + "\nIMSI:" + subscriberId
+                + "\nMCC[resources获取]:" + mcc
+                + "\nMNC[resources获取]:" + mnc
+                + "\n================手机其他信息==================="
+                + "\n电话状态:" + callState
+                + "\n电话方位:" + cellLoc
+                + "\nIMEI:" + imei
+                + "\n设备软件版本号:" + softwareVersion
+                + "\n手机号:" + lineNum
+//                + "\n附近电话信息:" + sb.toString()
+                + "\n注册的ISO国家注册码:" + networkCountryIso
+                + "\nnetworkOperator(MCC+MNC):" + networkOperator
+                + "\n(当前已注册的用户)的名字:" + networkOperatorName
+                + "\n当前使用的网络类型:" + networkType
+                + "\n手机类型:" + phoneType
+                + "\n语音邮件号码:" + VoiceMailNumber
+                + "\nICC卡是否存在:" + hasIccCard
+                + "\n是否漫游:" + isNetworkRoaming
+                + "\n数据活动状态:" + dataActivity
+                + "\n数据连接状态:" + dataState
+                ;
+        showMessage(s);
 
     }
 
@@ -441,5 +647,37 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+
+    /**
+     * 判断当前应用是否具有指定的权限
+     *
+     * @param context
+     * @param permission 权限信息的完整名称 如：<code>android.permission.INTERNET</code>
+     * @return 当前仅当宿主应用含有 参数 permission 对应的权限 返回true 否则返回 false
+     */
+    public static boolean checkPermission(Context context, String permission) {
+        boolean result = false;
+        if (Build.VERSION.SDK_INT >= 23) {
+            try {
+                Class<?> clazz = Class.forName("android.content.Context");
+                Method method = clazz.getMethod("checkSelfPermission", String.class);
+                int rest = (Integer) method.invoke(context, permission);
+                if (rest == PackageManager.PERMISSION_GRANTED) {
+                    result = true;
+                } else {
+                    result = false;
+                }
+            } catch (Throwable e) {
+                result = false;
+            }
+        } else {
+            PackageManager pm = context.getPackageManager();
+            if (pm.checkPermission(permission, context.getPackageName()) == PackageManager.PERMISSION_GRANTED) {
+                result = true;
+            }
+        }
+        return result;
+    }
 
 }
